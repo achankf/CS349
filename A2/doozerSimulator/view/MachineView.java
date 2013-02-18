deaacb096a246c4b5d718390399e2bd2eed26fef
Alfred Chan <alfred8844@gmail.com>
2013-02-18 17:51:21 -0500

    * rendering order changed:  draw objects then gameover/time text

--- A2/doozerSimulator/view/MachineView.java
+++ A2/doozerSimulator/view/MachineView.java
@@ -57,16 +57,6 @@ public class MachineView extends JComponent {
 
 		Graphics2D g2d = (Graphics2D) g;
 
-		g2d.setFont(new Font(null, Font.PLAIN, 18));
-		DecimalFormat df = new DecimalFormat("##.##");
-
-		if (model.gameOver()){
-			g2d.drawString("Game Over!", 50, 200);
-			g2d.drawString("Time Elapsed:" + df.format(model.getTimeElapsed()) + " seconds", 100, 250);
-		} else {
-			g2d.drawString("Time Elapsed:" + df.format(model.getTimeElapsed()) + " seconds", 0, 30);
-		}
-		
 		// call draw methods in model
 		model.drawAll(g2d, new Convert(scale){
 			public Point fromCanvas(Point pt){
@@ -76,6 +66,16 @@ public class MachineView extends JComponent {
 				return to(pt);
 			}
 		});
+
+		g2d.setFont(new Font(null, Font.PLAIN, 18));
+		DecimalFormat df = new DecimalFormat("##.##");
+
+		if (model.gameOver()){
+			g2d.drawString("Game Over!", 50, 200);
+			g2d.drawString("Time Elapsed:" + df.format(model.getTimeElapsed()) + " seconds", 100, 250);
+		} else {
+			g2d.drawString("Time Elapsed:" + df.format(model.getTimeElapsed()) + " seconds", 0, 30);
+		}
 	}
 
 	/** Convert from the model's X coordinate to the component's X coordinate. */

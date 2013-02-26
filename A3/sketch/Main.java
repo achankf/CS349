3bce33667f932753b833667869124b38024ac3c3
Alfred Chan <alfred8844@gmail.com>
2013-02-25 20:49:01 -0500

    * added main class and the view interface

---
+++ A3/sketch/Main.java
@@ -0,0 +1,9 @@
+package sketch;
+
+import sketch.model.*;
+
+public class Main{
+	public static void main(String []args){
+
+	}
+}
---
+++ A3/sketch/model/IView.java
@@ -0,0 +1,9 @@
+package sketch.model;
+
+public interface IView {
+	/**
+	 * Update the view. This method is generally called by the model whenever it
+	 * has changed state.
+	 */
+	public void updateView();
+}
